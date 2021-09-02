70a3054691e635a80fcf09cdde8adbe6dbb8e2d0
NanyDesu <eladesumenezes@gmail.com>
2021-09-02 13:35:15 -0300

    Update in Body.dart

--- artstreet/lib/pages/body.dart
+++ artstreet/lib/pages/body.dart
@@ -25,10 +25,42 @@ class Body extends StatelessWidget{
                 color: Colors.white, fontWeight: FontWeight.bold
                 ),
               ),
-
             ],
           ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            height: 54,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0,10),
+                  blurRadius: 50,
+                  color: Colors.black.withOpacity(0.23),
+                )
+              ],
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "Search",
+                hintStyle: TextStyle(
+                  color: Colors.pink.withOpacity(0.5),
+                  ),
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+              ),
+            ),
+            ),
           )
+
         ],),
       )
     ],
